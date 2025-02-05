93b8c24500ea06c7b921de888feea93bc2456026
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2025-02-05 22:40:20 +0200

    Add generic ToArray extension.

--- SqlServerExplorerLib/DataServices/DataTableExtensions.cs
+++ SqlServerExplorerLib/DataServices/DataTableExtensions.cs
@@ -16,6 +16,16 @@ public static class DataTableExtensions
     )];
     }
 
+    public static T?[] ToArray<T>(this DataTable table) where T : struct
+    {
+        return [.. table.AsEnumerable().Select(row =>
+    {
+        if (row[0] == DBNull.Value) return (T?)null;
+        return (T)row[0];
+    }
+    )];
+    }
+
     public static string[] ToStringArray(this DataTable table, bool useEmptyStringForNull)
     {
         return [.. table.AsEnumerable().Select(row =>
@@ -38,4 +48,15 @@ public static class DataTableExtensions
     }
     )];
     }
+
+    //public static List<int?> ToIntList(this DataTable table, bool useZeroForNull)
+    //{
+    //    return [.. table.AsEnumerable().Select(row =>
+    //{
+    //    if (row[0] == DBNull.Value)
+    //        return useZeroForNull ? 0 : (int?)null;
+    //    return (int)row[0];
+    //}
+    //)];
+    //}
 }
